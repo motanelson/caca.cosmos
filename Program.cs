4d28b66a1d6c259186a68a895482e3a45d179905
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-09-09 13:02:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿
+class graf 
+{
+    public static void Points(int x, int y)
+    {
+        Console.CursorLeft = x; Console.CursorTop = y;
+
+        Console.WriteLine(" ");
+    
+    
+    
+    
+    
+    }
+    public static void hlines(int x, int y, int x1) 
+    {
+
+        int i = 0;
+        for (int j = x; j < x1; j++) Points(j,y);
+    
+    
+    }
+    public static void vline(int x, int y, int y1) 
+    {
+
+        int i = 0;
+        for (int j = y; j < y1; j++) Points(x, j);
+
+
+
+    }
+
+    public static void box(int x, int y, int x1, int y1)
+    {
+
+        for (int j = y; j < y1; j++) hlines(x, j,x1);
+
+
+    }
+
+}
+
+
+
+
+
+
+class tests 
+
+
+
+{
+
+
+    public static void mainLoop() 
+    {
+        graf.box(1, 1, 10,10);
+       
+    
+    
+    }
+
+
+
+}
+
+
+
+class lines { 
+
+     public static void Main()
+    {
+
+        Console.BackgroundColor=ConsoleColor.White;
+        Console.ForegroundColor=ConsoleColor.Black;
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Black;
+        tests.mainLoop();
+        Console.BackgroundColor = ConsoleColor.White;
+
+
+    }
+
+
+
+}
+
